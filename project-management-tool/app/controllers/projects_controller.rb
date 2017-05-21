51dea84bfb13b16ccfcd95843cac704bd505eb12
Hano <hinsul3113@gmail.com>
2017-05-20 21:47:13 -0700

    First commit for project managment tool with Rails

---
+++ project-management-tool/app/controllers/projects_controller.rb
@@ -0,0 +1,48 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    project_params = params.require(:project).permit([:title, :description])
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_path(@project), notice: "Succesfully Created"
+    else
+      flash.now[:alert] = "Unable to Created"
+      render :new
+    end
+  end
+
+  def show
+  @project = Project.find params[:id]
+  # render json: params
+end
+
+  def destroy
+    @project = Project.find params[:id]
+    @project.destroy
+    redirect_to projects_path "Project Deleted"
+  end
+
+  def edit
+    @project = Project.find params[:id]
+  end
+
+  def update
+    @project = Project.find params[:id]
+    project_params = params.require('project').permit(:title, :description)
+
+    if @project.update(project_params)
+      redirect_to project_path(@project), notice: "Succesfully Updated"
+    else
+      flash.now[:alert] = "Unable to Updated"
+      render :edit
+    end
+  end
+end
---
+++ project-management-tool/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# get('/', to: 'welcome#home')
+# get('/projects/', to: 'projects#index')
+# get('/projects/:id', {to: 'projects#show', as: :project})
+get('/tasks/', to: 'tasks#index')
+get('/discussions/', to: 'discussions#index')
+get('/comments/', to: 'comments#index')
+#
+# get('/projects/new/', {to: 'projects#new', as: :new_project})
+# post('/projects', to: 'projects#create')
+
+resources :projects
+# get('/projects/:id/edit', {to: 'projects#edit', as: :edit_project})
+# patch('/projects/:id', {to: 'projects#update'})
+# delete('/projects/:id', {to: 'projects#destroy'})
+end
